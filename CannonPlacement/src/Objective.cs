bfaae2f1a160650c4cf67624b7a1da9023e33582
Florian Richoux <florian.richoux@polytechnique.edu>
2015-09-27 22:00:38 +0200

    Start CannonPlacement

---
+++ CannonPlacement/src/Objective.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CannonPlacement
+{
+  public class Objective
+  {
+    public Objective()
+    {
+    }
+  }
+}
+
---
+++ CannonPlacement/src/SetVariables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CannonPlacement
+{
+  public class SetVariables : ghost.SetVariables<Variable>
+  {
+    public SetVariables( List<Variable> variables, int width, int height )
+      : base( variables )
+    {
+      Grid = new int[width, height];
+    }
+
+    public int[,] Grid { get; set; } 
+  }
+}
+
